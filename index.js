1b2be10c2a335cc61503442e0bb5185492bb4569
anthony62490 <anthonyjmagno@gmail.com>
2019-07-07 14:31:43 -0500

    removing extra logs and commands

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-//Firebase/GCP modules
+//GCP modules
 const { Storage } = require('@google-cloud/storage');
+const spawn = require('child-process-promise').spawn;
 //OS utilities
 const os = require('os');
 const path = require('path'); 
 const envFilePath = path.join(__dirname, '../.env')
 require('dotenv').config({path: path.resolve(envFilePath)});
 
-const spawn = require('child-process-promise').spawn;
 const suffix = '-small';
 const projectId = process.env.PROJECT_ID;
 let gcs = new Storage ({projectId});
@@ -14,20 +14,13 @@ let gcs = new Storage ({projectId});
 
 exports.resizeImage = (event, context) => {
   if(context.eventType === "google.storage.object.finalize"){
-    console.log(`Event Overview ${event}`);
-    console.log(`Context Overview ${context}`);
+    console.log(`Event Overview ${JSON.stringify(event)}`);
     const bucket = event.bucket;
     console.log("Bucket", bucket);
     const contentType = event.contentType;
     console.log("ContentType", contentType);
     const filePath = event.name;
-    console.log('FIREBASE INDEX: File change detected. Executing function');
     console.log("filepath =", filePath);
-
-    if (event.resourceState && event.resourceState === 'not_exists') {
-      console.log('File deleted sucessfully');
-      return;
-    }
 
     if (path.basename(filePath).endsWith(suffix)) {
       console.log('File has already been altered. Take no action');
@@ -49,7 +42,3 @@ exports.resizeImage = (event, context) => {
     });
   }
 }
-
-// Deployment Command
-// 1. Select <PROJECT-NAME> with gcloud
-// 2. Run: gcloud functions deploy resizeImage --runtime nodejs8 --trigger-resource <PROJECT-NAME>.appspot.com --trigger-event google.storage.object.finalize
