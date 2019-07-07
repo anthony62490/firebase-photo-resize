2faaef5025f9a446092799f3df212f2b4e66fcf7
anthony62490 <anthonyjmagno@gmail.com>
2019-07-07 14:03:44 -0500

    updating function with new gcp interface

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || "Hello World!";
-  res.status(200).send(message);
-};
+//Firebase/GCP modules
+const { Storage } = require('@google-cloud/storage');
+//OS utilities
+const os = require('os');
+const path = require('path'); 
+const envFilePath = path.join(__dirname, '../.env')
+require('dotenv').config({path: path.resolve(envFilePath)});
+
+const spawn = require('child-process-promise').spawn;
+const suffix = '-small';
+const projectId = process.env.PROJECT_ID;
+let gcs = new Storage ({projectId});
+
+
+exports.resizeImage = (event, context) => {
+  if(context.eventType === "google.storage.object.finalize"){
+    console.log(`Event Overview ${event}`);
+    console.log(`Context Overview ${context}`);
+    const bucket = event.bucket;
+    console.log("Bucket", bucket);
+    const contentType = event.contentType;
+    console.log("ContentType", contentType);
+    const filePath = event.name;
+    console.log('FIREBASE INDEX: File change detected. Executing function');
+    console.log("filepath =", filePath);
+
+    if (event.resourceState && event.resourceState === 'not_exists') {
+      console.log('File deleted sucessfully');
+      return;
+    }
+
+    if (path.basename(filePath).endsWith(suffix)) {
+      console.log('File has already been altered. Take no action');
+      return;
+    }
+
+    const destBucket = gcs.bucket(bucket);
+    const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+    const metadata = { contentType: contentType };
+    return destBucket.file(filePath).download({
+      destination: tmpFilePath
+    }).then(() => {
+      return spawn('convert', [tmpFilePath, '-resize', '100x100', tmpFilePath]);
+    }).then(() => {
+      return destBucket.upload(tmpFilePath, {
+        destination: `images/${path.basename(filePath)}${suffix}`,
+        metadata: metadata
+      })
+    });
+  }
+}
+
+// Deployment Command
+// 1. Select <PROJECT-NAME> with gcloud
+// 2. Run: gcloud functions deploy resizeImage --runtime nodejs8 --trigger-resource <PROJECT-NAME>.appspot.com --trigger-event google.storage.object.finalize
